ed281edab416b7d9fd0aa42a4bb099c01c506f74
jaromhulet <jarom.hulet@gmail.com>
2026-06-02 23:25:57 -0500

    Create run_monte_carlo.py

---
+++ run_monte_carlo.py
@@ -0,0 +1,26 @@
+from 
+
+# Run the Monte Carlo
+list_of_circles = []
+num_strings = 50
+num_simulations = 10000
+
+if __name__ == "__main__":
+  for _ in range(0, num_simulations):
+      
+      # create the simulated starting box of strings
+      strings = create_strings(num_strings)
+  
+      # start circle counter for this simulation
+      circle_counter = 0
+  
+      # draw from the box until there are no more strings left
+      while len(strings) > 0:
+          end_1, end_2, strings = select_ends(strings)
+          strings, circle_bool = tie_ends(strings, end_1, end_2)
+          circle_counter += circle_bool
+          
+      # add the number of circles that counts number of circles for each round
+      list_of_circles.append(circle_counter)
+  
+  print(np.mean(list_of_circles))
